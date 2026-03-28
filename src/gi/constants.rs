573cbf1303d96e768a3b39b99111681dc059ace3
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2026-03-28 12:28:50 +0100

    Set probe size to 4

--- src/gi/constants.rs
+++ src/gi/constants.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 
 use crate::gi::compositing::PostProcessingMaterial;
 
-pub const GI_SCREEN_PROBE_SIZE: i32 = 8;
+pub const GI_SCREEN_PROBE_SIZE: i32 = 4;
 
 pub const POST_PROCESSING_RECT: Handle<Mesh> = uuid_handle!("00000000-0000-0001-45ca-30fd30a6002b");
 pub const POST_PROCESSING_MATERIAL: Handle<PostProcessingMaterial> =
